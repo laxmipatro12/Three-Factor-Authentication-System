bf3a78e3a53401bbad991a3316217319eef9c21c
Laxmi <patrosanjana7@gmail.com>
2022-01-13 12:26:33 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+ include('config.php');
+
+$insert = false;
+if(isset($_SESSION['name'])){
+
+
+ // Collect post variables
+$name =$_SESSION['name'];
+$password=$_SESSION['pass'];
+$hashed=hash('sha256',$password);
+$email =$_SESSION['email'];
+$birthday =$_SESSION['birthday'];
+$number =$_SESSION['phonenumber'] ;
+$gender=$_SESSION['gender'];
+$address=$_SESSION['address'];
+
+
+$vehicle=$_POST['vehicle'];
+$lucky=$_POST['lucky'];
+// last question
+$last=$_POST['last'];
+
+
+$sql="INSERT INTO `cse3501proj`.`userdetails` (`name`, `password`, `email`, `birthday`, `number`, `gender`, `address`, `vehicle`, `lucky`, `last`, `timestamp`)
+ VALUES ('$name', '$hashed', '$email', '$birthday', '$number', '$gender', '$address', '$vehicle','$lucky','$last',current_timestamp())";
+
+ if($con->query($sql) == true){
+
+   $insert=true;
+  echo "<center><h1>Registration Succesfull!<h1><center>";
+  print("<img src=images/thank-removebg-preview.png>");
+}
+else{
+    echo "Error:<br> $con->error";
+}
+
+ $con->close();
+ }
